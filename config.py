75f2362166c3d12c1f372126fa65577489af45b6
telegrambot622 <158012060+telegrambot622@users.noreply.github.com>
2024-03-16 06:45:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ from pyrogram import filters
 import os
 
 class Config:
-    API_ID = "24970012"
-    API_HASH = "43804289ce8eb7e37a1d680199cd2607"
+    API_ID = "23378380"
+    API_HASH = "5ddcf1df1360fe446b06dd94bd3ad0c6"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     TOKEN = os.environ.get("TOKEN", None)
     MONGO_URL = "mongodb+srv://jarvisxd45:rajnish68mishra85@atlascluster.xkizs7c.mongodb.net/?retryWrites=true&w=majority"
-    START_PIC = "https://telegra.ph/file/0eba143d65f9413f9ae04.jpg"
-    SUDOERS = filters.user(["7157587567"])
+    START_PIC = "https://telegra.ph/file/2397df5c916a1933957d4.jpg"
+    SUDOERS = filters.user(["6539801772"])
