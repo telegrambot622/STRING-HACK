5ded34b271f28d48389e64e92b7203884e807a0b
telegrambot622 <158012060+telegrambot622@users.noreply.github.com>
2024-03-25 11:23:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config:
     API_HASH = "5ddcf1df1360fe446b06dd94bd3ad0c6"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://jarvisxd45:rajnish68mishra85@atlascluster.xkizs7c.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_URL = "mongodb+srv://JARVISV1:JARVIS85@annie.bfgvyxi.mongodb.net/?retryWrites=true&w=majority&appName=Annie"
     START_PIC = "https://telegra.ph/file/2397df5c916a1933957d4.jpg"
-    SUDOERS = filters.user(["6539801772"])
+    SUDOERS = filters.user(["7110021757"])
